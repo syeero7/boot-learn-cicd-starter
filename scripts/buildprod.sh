6bc53270dce3cbf944aa6a9ad73697f90c9a0b46
emal <emalhasitha@proton.me>
2025-11-11 14:49:36 +0530

    Revert "chore: log error info on failure"
    This reverts commit 92b5a5bbd41a13ecac64740c91fcc4218a07d160.

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-set +e
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o notely 2>error.log
-EXIT_CODE=$?
-
-if [ $EXIT_CODE -ne 0 ]; then
-  echo "BUILD_FAILED_MARKER" >>error.log
-  exit $EXIT_CODE
-fi
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o notely
