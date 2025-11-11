92b5a5bbd41a13ecac64740c91fcc4218a07d160
emal <emalhasitha@proton.me>
2025-11-11 14:45:59 +0530

    chore: log error info on failure

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o notely
+set +e
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o notely 2>error.log
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "BUILD_FAILED_MARKER" >>error.log
+  exit $EXIT_CODE
+fi
